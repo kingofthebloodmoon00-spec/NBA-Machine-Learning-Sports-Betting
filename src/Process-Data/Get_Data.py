ae324a8819d3e6fc8fca10a6ab0142878e4f7a8a
kingofthebloodmoon00-spec <kingofthebloodmoon00@gmail.com>
2025-11-08 22:21:53 -0800

    Get_Data.py

--- src/Process-Data/Get_Data.py
+++ src/Process-Data/Get_Data.py
@@ -24,23 +24,33 @@ url = config['data_url']
 
 con = sqlite3.connect(os.path.join(repo_root, "Data/TeamData.sqlite"))
 
-for key, value in config['get-data'].items():
-    date_pointer = datetime.strptime(value['start_date'], "%Y-%m-%d").date()
-    end_date = datetime.strptime(value['end_date'], "%Y-%m-%d").date()
+# Iterate seasons
+for season_key, season_table in config['get-data'].items():
+    # season_table might be nested if using dotted keys
+    for subkey, value in season_table.items() if isinstance(season_table, dict) else [(season_key, season_table)]:
+        # Auto-detect date format
+        start_date_str = value['start_date']
+        end_date_str = value['end_date']
 
-    while date_pointer <= end_date:
-        print("Getting data: ", date_pointer)
+        date_format = "%Y-%m-%d"
+        if "/" in start_date_str:
+            date_format = "%m/%d/%Y"
 
-        raw_data = get_json_data(
-            url.format(date_pointer.month, date_pointer.day, value['start_year'], date_pointer.year, key))
-        df = to_data_frame(raw_data)
+        date_pointer = datetime.strptime(start_date_str, date_format).date()
+        end_date = datetime.strptime(end_date_str, date_format).date()
 
-        date_pointer = date_pointer + timedelta(days=1)
+        while date_pointer <= end_date:
+            print("Getting data:", date_pointer)
 
-        df['Date'] = str(date_pointer)
+            raw_data = get_json_data(
+                url.format(date_pointer.month, date_pointer.day, value['start_year'], date_pointer.year, season_key)
+            )
+            df = to_data_frame(raw_data)
 
-        df.to_sql(date_pointer.strftime("%Y-%m-%d"), con, if_exists="replace")
+            df['Date'] = str(date_pointer)
+            df.to_sql(date_pointer.strftime("%Y-%m-%d"), con, if_exists="replace")
 
-        time.sleep(random.randint(1, 3))
+            date_pointer += timedelta(days=1)
+            time.sleep(random.randint(1, 3))
 
 con.close()
